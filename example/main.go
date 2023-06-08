f52bc972ba7e3da3326132529498ddb2d0a62ed3
test <test@google.com>
2023-06-08 09:32:37 +0800

    v1.0.0

--- example/main.go
+++ example/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	xxl "github.com/xxl-job/xxl-job-executor-go"
-	"github.com/xxl-job/xxl-job-executor-go/example/task"
+	xxl "github.com/Nname/xxl-job-executor-go"
+	"github.com/Nname/xxl-job-executor-go/example/task"
 	"log"
 )
 
